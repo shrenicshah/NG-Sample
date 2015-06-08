531cad0155c3ab8d595c330aad95320c18c99d88
shrenicshah <shrenic.shah@gmail.com>
2015-06-08 12:32:37 +1000

    Inital commit

---
+++ Contacts/App/clientRoute.js
@@ -0,0 +1,22 @@
+﻿contactApp.config(['$routeProvider', function ($routeProvider) {
+    
+    $routeProvider.when('/mycontacts', {
+        templateUrl: "app/contact/html/contactsList.html",
+        controller: "contactController"
+    }),
+
+    $routeProvider.when('/mycontacts/newcontact', {
+        templateUrl: "app/contact/html/contactForm.html",
+        controller: "contactAddController"
+    }),
+    
+    $routeProvider.when('/mycontacts/:id', {
+        templateUrl: "app/contact/html/contactForm.html",
+        controller: "contactEditController"
+    }),
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+
+}]);
